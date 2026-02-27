f096fedf8cd871854e3a523056c56509b5581b6d
hp <hp@LAPTOP-D4L4O9D3>
2026-02-28 04:35:47 +0530

    simple

--- GrocerryApplicationProject/src/main/java/constants/Constants.java
+++ GrocerryApplicationProject/src/main/java/constants/Constants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class Constants {
 	
-	public static final String TESTDATAFILE = System.getProperty("user.dir") + "\\src\\test\\resources\\TestData.xlsx";
+	public static final String TESTDATAFILE = System.getProperty("user.dir") + "\\src\\test\\resources\\Testdata.xlsx";
 	public static final String CONFIGFILE = System.getProperty("user.dir") + "\\src\\main\\resources\\config.properties";
 	
 	public static final String VALIDCREDENTIALERROR = "user was unable to login with valid credentials";
